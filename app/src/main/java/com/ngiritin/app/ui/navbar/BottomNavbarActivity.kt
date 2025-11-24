fc2f7c3405c48110bfe600272c9825495394d9f9
oktaavsm <incdmincs@gmail.com>
2025-11-25 06:19:41 +0700

    feat: add bottom navbar activity
    Adds the initial files for a bottom navigation bar feature. This includes:
    - `BottomNavbarActivity.kt` class
    - `activity_bottom_navbar.xml` layout file
    - Declaration of the new activity in `AndroidManifest.xml`

---
+++ app/src/main/java/com/ngiritin/app/ui/navbar/BottomNavbarActivity.kt
@@ -0,0 +1,21 @@
+package com.ngiritin.app.ui.navbar
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.ngiritin.app.R
+
+class BottomNavbarActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_bottom_navbar)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
